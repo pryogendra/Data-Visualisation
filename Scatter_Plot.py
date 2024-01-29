a3b9130fcfe7cec160d0bf346485265ecd6e2148
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2024-01-29 19:34:03 +0530

    Python

---
+++ Scatter_Plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x=list("123456789011223344556677889900")
+y=list("123456789000998877665544332211")
+plt.scatter(x,y)
+plt.title("Scatter Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
