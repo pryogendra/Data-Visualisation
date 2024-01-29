4d004dcd2df17a8095386e1cf8b52f156e9872e0
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2024-01-29 19:06:42 +0530

    Python
    Bar Graph

---
+++ bar_chart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+cat=['A','B','C','D','E']
+val=[20,40,12,23,45]
+plt.bar(cat,val)
+plt.xlabel("Categories")
+plt.ylabel("Values")
+plt.title("Bar Chart")
+plt.show()
